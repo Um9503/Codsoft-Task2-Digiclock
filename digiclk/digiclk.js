4169e3332263d4f766d1b8d66a58f16dc21fc5f4
Umaima Ali <140103366+Um9503@users.noreply.github.com>
2023-12-17 01:57:59 +0530

    Add files via upload

---
+++ digiclk/digiclk.js
@@ -0,0 +1,19 @@
+function updateClock() {
+    const now = new Date();
+  
+    const hours = formatTimeUnit(now.getHours());
+    const minutes = formatTimeUnit(now.getMinutes());
+    const seconds = formatTimeUnit(now.getSeconds());
+  
+    document.getElementById('hours').textContent = hours;
+    document.getElementById('minutes').textContent = minutes;
+    document.getElementById('seconds').textContent = seconds;
+  }
+  
+  function formatTimeUnit(unit) {
+    return unit < 10 ? '0' + unit : unit;
+  }
+    setInterval(updateClock, 1000);
+    updateClock();
+  
+    
